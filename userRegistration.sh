5fffe1dc4fd952658b4e9815ef25f066f85860f8
chetan-rote <rote.chetan@gmail.com>
2020-08-29 22:39:56 +0530

    Validates User First name

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,4 +2,17 @@
 
 #!/bin/bash
 
+#UC1 validate First Name
+shopt -s extglob
 
+pat="^[[:upper:]]{1}[[:lower:]]{2,8}$";
+
+#user input
+read -p "Enter First Name:" fname
+
+if [[ $fname =~ $pat ]]
+then
+        echo "First Name is valid"
+else
+        echo "First Name is not valid"
+fi
