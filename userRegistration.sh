a6b42a4ef2faabd738ed05fd6ac85f68cb040a06
chetan-rote <rote.chetan@gmail.com>
2020-08-29 22:45:46 +0530

    Validate User's Last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -16,3 +16,17 @@ then
 else
         echo "First Name is not valid"
 fi
+
+#UC2 validate Last Name
+
+pat="^[[:upper:]]{1}[[:lower:]]{2,8}$";
+
+#user input
+read -p "Enter Last Name:" lname
+
+if [[ $lname =~ $pat ]]
+then
+        echo "Last Name is valid"
+else
+        echo "Last Name is not valid"
+fi
